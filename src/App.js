225f088a62d4e3826d1bd3e57f564f418ad96c11
Thiago <thiago-470@outlook.com>
2022-10-27 00:32:48 -0300

    requisito 05

--- src/App.js
+++ src/App.js
@@ -28,6 +28,33 @@ class App extends React.Component {
 
     this.setState({
       [name]: value,
+    }, () => {
+      const {
+        cardName,
+        cardDescription,
+        cardImage,
+        cardRare,
+        cardAttr1,
+        cardAttr2,
+        cardAttr3,
+      } = this.state;
+      const pontosmax = 210;
+      const atributomax = 90;
+      const total = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+
+      const attt1MaxCheck = (cardAttr1 <= atributomax
+      && cardAttr1 >= 0);
+      const attt2MaxCheck = (cardAttr2 <= atributomax
+      && cardAttr2 >= 0);
+      const attt3MaxCheck = (cardAttr3 <= atributomax
+      && cardAttr3 >= 0);
+      const ChecandoTudo = attt1MaxCheck && attt2MaxCheck && attt3MaxCheck;
+
+      const checarTexto = cardName && cardDescription && cardImage && cardRare;
+      const checarAtributo = (total <= pontosmax);
+
+      this.setState({ isSaveButtonDisabled: !(ChecandoTudo && checarAtributo
+        && checarTexto) });
     });
   }
 
